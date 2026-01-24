5ab69c2dea4f153009ee7e91e07cbcd817ab453d
jacyeboost <jac.ye@outlook.com>
2026-01-24 09:37:16 -0600

    quité topbar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
+//import Topbar from '../components/Topbar';
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -17,7 +17,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, onHistoryClick, onFea
 
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col h-screen overflow-hidden">
-        <Topbar onShare={onShare} />
+        {/* <Topbar onShare={onShare} /> */}
         
         <main className="flex-1 overflow-y-auto p-6 scroll-smooth">
           <div className="max-w-[1600px] mx-auto">
